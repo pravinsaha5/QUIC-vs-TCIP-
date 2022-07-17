bd7bfe993d01f08ae0076f7c36d0208ab77ebd11
pravinsaha5 <pravinsaha5@gmail.com>
2022-07-17 18:14:11 +0530

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,33 +1,21 @@
 #!/bin/bash
 
-# python run_benchmark.py -p QUIC TCP -l 0.0 -d 10 -v 0 -b 100 --vverbose
-# python run_benchmark.py -p QUIC TCP -l 0.0 -d 10 -v 0 -b 40 --vverbose
-# python run_benchmark.py -p QUIC TCP -l 0.0 -d 10 -v 0 -b 5 --vverbose
+python comparison.py -p QUIC TCP -l 5.0 -d 50 -v 0 -b 100 --vverbose
+python comparison.py -p QUIC TCP -l 5.0 -d 50 -v 0 -b 40 --vverbose
+python comparison.py -p QUIC TCP -l 5.0 -d 50 -v 0 -b 5 --vverbose
 
-# python run_benchmark.py -p QUIC TCP -l 5.0 -d 10 -v 0 -b 100 --vverbose
-# python run_benchmark.py -p QUIC TCP -l 5.0 -d 10 -v 0 -b 40 --vverbose
-# python run_benchmark.py -p QUIC TCP -l 5.0 -d 10 -v 0 -b 5 --vverbose
+python comparison.py -p QUIC TCP -l 0.0 -d 10 -v 0 -b 100 -s 1 --vverbose
+python comparison.py -p QUIC TCP -l 0.0 -d 10 -v 0 -b 40 -s 1 --vverbose
+python comparison.py -p QUIC TCP -l 0.0 -d 10 -v 0 -b 5 -s 1 --vverbose
 
-# python run_benchmark.py -p QUIC TCP -l 0.0 -d 50 -v 0 -b 100 --vverbose
-# python run_benchmark.py -p QUIC TCP -l 0.0 -d 50 -v 0 -b 40 --vverbose
-# python run_benchmark.py -p QUIC TCP -l 0.0 -d 50 -v 0 -b 5 --vverbose
+python comparison.py -p QUIC TCP -l 5.0 -d 10 -v 0 -b 100 -s 1 --vverbose
+python comparison.py -p QUIC TCP -l 5.0 -d 10 -v 0 -b 40 -s 1 --vverbose
+python comparison.py -p QUIC TCP -l 5.0 -d 10 -v 0 -b 5 -s 1 --vverbose
 
-python run_benchmark.py -p QUIC TCP -l 5.0 -d 50 -v 0 -b 100 --vverbose
-python run_benchmark.py -p QUIC TCP -l 5.0 -d 50 -v 0 -b 40 --vverbose
-python run_benchmark.py -p QUIC TCP -l 5.0 -d 50 -v 0 -b 5 --vverbose
+python comparison.py -p QUIC TCP -l 0.0 -d 50 -v 0 -b 100 -s 1 --vverbose
+python comparison.py -p QUIC TCP -l 0.0 -d 50 -v 0 -b 40 -s 1 --vverbose
+python comparison.py -p QUIC TCP -l 0.0 -d 50 -v 0 -b 5 -s 1 --vverbose
 
-python run_benchmark.py -p QUIC TCP -l 0.0 -d 10 -v 0 -b 100 -s 1 --vverbose
-python run_benchmark.py -p QUIC TCP -l 0.0 -d 10 -v 0 -b 40 -s 1 --vverbose
-python run_benchmark.py -p QUIC TCP -l 0.0 -d 10 -v 0 -b 5 -s 1 --vverbose
-
-python run_benchmark.py -p QUIC TCP -l 5.0 -d 10 -v 0 -b 100 -s 1 --vverbose
-python run_benchmark.py -p QUIC TCP -l 5.0 -d 10 -v 0 -b 40 -s 1 --vverbose
-python run_benchmark.py -p QUIC TCP -l 5.0 -d 10 -v 0 -b 5 -s 1 --vverbose
-
-python run_benchmark.py -p QUIC TCP -l 0.0 -d 50 -v 0 -b 100 -s 1 --vverbose
-python run_benchmark.py -p QUIC TCP -l 0.0 -d 50 -v 0 -b 40 -s 1 --vverbose
-python run_benchmark.py -p QUIC TCP -l 0.0 -d 50 -v 0 -b 5 -s 1 --vverbose
-
-python run_benchmark.py -p QUIC TCP -l 5.0 -d 50 -v 0 -b 100 -s 1 --vverbose
-python run_benchmark.py -p QUIC TCP -l 5.0 -d 50 -v 0 -b 40 -s 1 --vverbose
-python run_benchmark.py -p QUIC TCP -l 5.0 -d 50 -v 0 -b 5 -s 1 --vverbose
+python comparison.py -p QUIC TCP -l 5.0 -d 50 -v 0 -b 100 -s 1 --vverbose
+python comparison.py -p QUIC TCP -l 5.0 -d 50 -v 0 -b 40 -s 1 --vverbose
+python comparison.py -p QUIC TCP -l 5.0 -d 50 -v 0 -b 5 -s 1 --vverbose
