687614857baaddb410d4ab604f71aa9f4ca3916f
pravinsaha5 <pravinsaha5@gmail.com>
2022-07-17 18:17:04 +0530

    Delete plot_wprofx.py

--- plot_wprofx.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-xpoints = np.array([15.67, 20.88, 21.66])
-ypoints = np.array(["facebook", "google", "cloudflare"])
-
-plt.plot(xpoints, ypoints)
-plt.show()
