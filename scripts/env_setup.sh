67202e07507e9b51d9367ca5026d5edd419eacce
pravinsaha5 <pravinsaha5@gmail.com>
2022-07-17 18:43:02 +0530

    Update env_setup.sh

--- scripts/env_setup.sh
+++ scripts/env_setup.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-mkdir -p raw processed plt tmp
+mkdir -p raw processed plt tmp                           # Creating directories 
 
-sudo ifconfig lo mtu 1500
-sudo sysctl -w net.ipv6.conf.lo.disable_ipv6=1
+sudo ifconfig lo mtu 1500                                # dafault value for MTU over the network
 
-/home/csg/software/proto-quic/src/out/Default/quic_server --quic_response_cache_dir=/var/www/html --certificate_file=/home/csg/software/proto-quic/src/net/tools/quic/certs/out/leaf_cert.pem --key_file=/home/csg/software/proto-quic/src/net/tools/quic/certs/out/leaf_cert.pkcs8
+/home/csg/software/proto-quic/src/out/Default/quic_server --quic_response_cache_dir=/var/www/html --certificate_file=/home/csg/software/proto-quic/src/net/tools/quic/certs/out/leaf_cert.pem --key_file=/home/csg/software/proto-quic/src/net/tools/quic/certs/out/leaf_cert.pkcs8     # QUIC server setup
